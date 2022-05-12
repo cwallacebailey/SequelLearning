53a83c89c6f3fc0f13b2ab17cf0424b5f9de284d
cwallacebailey <cwallacebailey@gmail.com>
2022-05-12 07:44:29 +0000

    Need to start SQLAlchemy

---
+++ sql-psycopg2.py
@@ -0,0 +1,24 @@
+import psycopg2
+
+# connect to "chinook" database
+connection = psycopg2.connect(database="chinook")
+
+# build a cursor object of the database
+cursor = connection.cursor()
+
+# Query 1
+# cursor.execute('SELECT * FROM "Artist"')
+cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# fetch the results(multiple)
+results = cursor.fetchall()
+
+# fetch the result (single)
+# results = cursor.fetchone()
+
+# close the connection
+connection.close()
+
+# print results
+for result in results:
+    print(result)
